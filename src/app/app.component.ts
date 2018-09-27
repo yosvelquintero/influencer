2d2cbd9900363605bec8af6f1a5b77f4580e58bb
Yosvel <yosvel@anymindgroup.com>
2018-09-27 14:29:32 +0700

    Removed comments..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,12 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-// import { TabsPage } from '../pages/tabs/tabs';
 import { WelcomePage } from '../pages/welcome/welcome';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  // rootPage:any = TabsPage;
   rootPage: any = WelcomePage;
 
   constructor(
